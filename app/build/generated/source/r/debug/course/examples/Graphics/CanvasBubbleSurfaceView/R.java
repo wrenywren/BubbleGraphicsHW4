e5ed2b1310ec264b006871d26171c75f5f3a6e3c
wrenywren <wren.chance@gmail.com>
2016-11-22 14:03:15 -0500

    On touch event bubble appears at random location, decreases radius by 10 % and increases speed by 10 %

--- app/build/generated/source/r/debug/course/examples/Graphics/CanvasBubbleSurfaceView/R.java
+++ app/build/generated/source/r/debug/course/examples/Graphics/CanvasBubbleSurfaceView/R.java
@@ -23,11 +23,11 @@ public final class R {
     }
     public static final class id {
         public static final int frame=0x7f060000;
-        public static final int textView_Layout=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int text_view=0x7f030001;
+        public static final int score_text_view=0x7f030001;
+        public static final int text_view=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
--- app/src/main/java/course/examples/Graphics/CanvasBubbleSurfaceView/BubbleActivity.java
+++ app/src/main/java/course/examples/Graphics/CanvasBubbleSurfaceView/BubbleActivity.java
@@ -1,7 +1,10 @@
 package course.examples.Graphics.CanvasBubbleSurfaceView;
 
-import java.util.LinkedList;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.graphics.Matrix;
 import android.os.Handler;
 
 import android.app.Activity;
@@ -12,16 +15,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.DisplayMetrics;
-import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import static android.content.ContentValues.TAG;
+import android.widget.Toast;
 
 /* @author A. Porter
  * Revised by S. Anderson
@@ -31,8 +36,12 @@ public class BubbleActivity extends Activity {
 	BubbleView mBubbleView;
     RelativeLayout relativeLayout;
     TextView frameRate;
+    TextView score;
     double startTime;
     int frameCount = 0;
+    int scoreCount = 0;
+    double scoreStartTime;
+
 
 
 	/** Simply create layout and view. */
@@ -47,9 +56,15 @@ public class BubbleActivity extends Activity {
 				BitmapFactory.decodeResource(getResources(), R.drawable.b256));
 		relativeLayout.addView(bubbleView);
 
+
         // Create text View for Frame Rate
-        frameRate = (TextView) getLayoutInflater().inflate(R.layout.text_view,null);
+        frameRate = (TextView) getLayoutInflater().inflate(R.layout.text_view, null);
 		relativeLayout.addView(frameRate);
+        setContentView(relativeLayout);
+
+        //Create text View for Score
+        score = (TextView) getLayoutInflater().inflate(R.layout.score_text_view,null);
+        relativeLayout.addView(score);
         setContentView(relativeLayout);
 	}
 
@@ -68,7 +83,7 @@ public class BubbleActivity extends Activity {
     private Runnable mUpdateTask = new Runnable() {
         @Override
         public void run() {
-            calculateFPS(); //update();
+            //calculateFPS(); //update();
             //posts the runnable after 500msec
             mHandler.postDelayed(mUpdateTask, 500);
         }
@@ -87,7 +102,6 @@ public class BubbleActivity extends Activity {
 
 
 
-
     /*
 	  SurfaceView is dedicated drawing surface in the view hierarchy.
       SurfaceHolder.Callback determines changes to SurfaceHolder via surfaceXXX
@@ -96,12 +110,14 @@ public class BubbleActivity extends Activity {
 	private class BubbleView extends SurfaceView implements
 			SurfaceHolder.Callback {
 
-		private final Bitmap mBitmap;
-		private final int mBitmapHeightAndWidth, mBitmapHeightAndWidthAdj;
+		private Bitmap mBitmap;
+        private Bitmap resizedBitmap;
+		private int mBitmapHeightAndWidth;
+		private int mBitmapHeightAndWidthAdj;
 		private final DisplayMetrics mDisplay;
 		private final int mDisplayWidth, mDisplayHeight;
 		private float mX, mY, mDx, mDy, mRotation;
-		private final SurfaceHolder mSurfaceHolder;
+		private SurfaceHolder mSurfaceHolder;
 		private final Paint mPainter = new Paint(); // control style and color
 		private Thread mDrawingThread;
 
@@ -113,28 +129,46 @@ public class BubbleActivity extends Activity {
 		}
 
 
+        Boolean touchEvent = false;
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            scoreCount++;
+            score.setText(Integer.toString(scoreCount));
+
+            increaseSpeedByTenPercent();
+            Double d = 0.10; //Percentage to increase speed by
+            mBitmapHeightAndWidth = (int) (mBitmapHeightAndWidth - (mBitmapHeightAndWidth * d)); //decrease radius by 10%
+            mBitmap = getResizedBitmap(mBitmap,mBitmapHeightAndWidth, mBitmapHeightAndWidth);
+            touchEvent = true;
+            return super.onTouchEvent(event);
+        }
+        public void increaseSpeedByTenPercent(){
+            Double d = 0.10; //Percentage to increase speed by
+            mBitmapHeightAndWidthAdj = (int) ( mBitmapHeightAndWidthAdj + (mBitmapHeightAndWidthAdj * d)); //increase speed by 10 percent
+        }
+
+
         public BubbleView(Context context, Bitmap bitmap) {
 			super(context);
 
-			mBitmapHeightAndWidth = (int) getResources().getDimension(
-					R.dimen.image_height);
-			this.mBitmap = Bitmap.createScaledBitmap(bitmap,
-					mBitmapHeightAndWidth/5, mBitmapHeightAndWidth/5, false);
+            mBitmapHeightAndWidth = (int) getResources().getDimension(R.dimen.image_height);
+            this.mBitmap = Bitmap.createScaledBitmap(bitmap,mBitmapHeightAndWidth/1, mBitmapHeightAndWidth/1,false);
 
-			mBitmapHeightAndWidthAdj = mBitmapHeightAndWidth / 2;
+
+            mBitmapHeightAndWidthAdj = mBitmapHeightAndWidth / 2; //Changed from 2 to reduce movement
+			//for testing
 
 			mDisplay = new DisplayMetrics();
 			// get display width/height
-			BubbleActivity.this.getWindowManager().getDefaultDisplay()
-					.getMetrics(mDisplay);
+			BubbleActivity.this.getWindowManager().getDefaultDisplay().getMetrics(mDisplay);
 			mDisplayWidth = mDisplay.widthPixels;
 			mDisplayHeight = mDisplay.heightPixels;
 
-			// Give bubble random coords and speed at creation
+			// Give bubble random cords and speed at creation
 			Random r = new Random();
 			mX = (float) r.nextInt(mDisplayHeight);
 			mY = (float) r.nextInt(mDisplayWidth);
-			mDx = (float) r.nextInt(mDisplayHeight) / mDisplayHeight;
+			mDx = (float) r.nextInt(mDisplayHeight) / mDisplayHeight; // movement
 			mDx *= r.nextInt(2) == 1 ? MOVE_STEP : -1 * MOVE_STEP;
 			mDy = (float) r.nextInt(mDisplayWidth) / mDisplayWidth;
 			mDy *= r.nextInt(2) == 1 ? MOVE_STEP : -1 * MOVE_STEP;
@@ -150,14 +184,53 @@ public class BubbleActivity extends Activity {
 
 		/** drawing and rotation */
 		private void drawBubble(Canvas canvas) {
-            //frames++;
 			canvas.drawColor(Color.DKGRAY);
 			mRotation += ROT_STEP;
-			canvas.rotate(mRotation, mY + mBitmapHeightAndWidthAdj, mX
-					+ mBitmapHeightAndWidthAdj);
+			canvas.rotate(mRotation, mY + mBitmapHeightAndWidthAdj, mX + mBitmapHeightAndWidthAdj);
 			canvas.drawBitmap(mBitmap, mY, mX, mPainter);
             frameCount++; //increment frameRate!!
 		}
+
+
+        //Function to randomly re-draw bubble at location
+        private void drawBubbleAtRandom(Canvas canvas) {
+
+            Random r = new Random();
+            mX = (float) r.nextInt(mDisplayHeight);
+            mY = (float) r.nextInt(mDisplayWidth);
+            mDx = (float) r.nextInt(mDisplayHeight) / mDisplayHeight;
+            mDx *= r.nextInt(2) == 1 ? MOVE_STEP : -1 * MOVE_STEP;
+            mDy = (float) r.nextInt(mDisplayWidth) / mDisplayWidth;
+            mDy *= r.nextInt(2) == 1 ? MOVE_STEP : -1 * MOVE_STEP;
+            mDx *= 0.1;
+            mDy *= 0.1;
+            mRotation = 1.0f;
+
+            frameCount++;
+
+        }
+
+        //Function to resize current Bitmap
+        public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth)
+        {
+            if (newHeight <= 0 || newWidth <= 0){
+                Toast.makeText(BubbleActivity.this,"Bubble Is Too Small To See", Toast.LENGTH_SHORT).show();
+                return mBitmap;
+            }
+            int width = bm.getWidth();
+            int height = bm.getHeight();
+            float scaleWidth = ((float) newWidth) / width;
+            float scaleHeight = ((float) newHeight) / height;
+            // create a matrix for the manipulation
+            Matrix matrix = new Matrix();
+            // resize the bit map
+            matrix.postScale(scaleWidth, scaleHeight);
+            // recreate the new Bitmap
+            Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+            return resizedBitmap;
+        }
+
+
 
 		/** True iff bubble can move. */
 		private boolean move() {
@@ -187,23 +260,34 @@ public class BubbleActivity extends Activity {
 		/** When surface created, this creates its thread AND starts it running. */
 		@Override
 		public void surfaceCreated(SurfaceHolder holder) {
-			// Run as separate thread.
-			mDrawingThread = new Thread(new Runnable() {
-				public void run() {
-                    startTime = (int) System.currentTimeMillis();
-                    Canvas canvas = null;
-					// While bubble within view, lock and draw.
-					while (!Thread.currentThread().isInterrupted() && move()) {
-						canvas = mSurfaceHolder.lockCanvas();
-						if (null != canvas) { // Lock canvas while updating bitmap
-							drawBubble(canvas);
-							mSurfaceHolder.unlockCanvasAndPost(canvas);
-						}
-					}
-				}
-			});
-			mDrawingThread.start();
-		}
+                // Run as separate thread.
+                mDrawingThread = new Thread(new Runnable() {
+                    public void run() {
+                        startTime = (int) System.currentTimeMillis();
+                        scoreStartTime = (int) System.currentTimeMillis();
+                        Canvas canvas = null;
+                        Random r = new Random();
+                        // While bubble within view, lock and draw.
+                        while (!Thread.currentThread().isInterrupted() && move()) {
+                            canvas = mSurfaceHolder.lockCanvas();
+                            if (null != canvas) { // Lock canvas while updating bitmap
+                                drawBubble(canvas);
+                                mSurfaceHolder.unlockCanvasAndPost(canvas);
+                            }
+                            //When the screen is touched it makes bubble appear in random location
+                            if (touchEvent == true) {
+                                drawBubbleAtRandom(canvas);
+                                touchEvent = false;
+                            }
+                        }
+                    }
+
+
+
+                });
+                mDrawingThread.start();
+            }
+
 		/** Surface destroyed; stop thread. */
 		@Override
 		public void surfaceDestroyed(SurfaceHolder holder) {
